0a8744959ba23dbe73a286ca3e40797f7972e8f2
Yash Bagul <yashbagul100@gmail.com>
2026-04-06 21:07:37 +0530

    new start api

--- src/app.js
+++ src/app.js
@@ -13,6 +13,12 @@ app.use(cookieParser());
 app.use(express.json());
 
 /* use Routes */
+app.get('/',(req, res)=>{
+    res.status(200).json({
+        message : "Welcome to the Ledger API ,  Backend Project for the Ledger System"
+    })
+})
+
 app.use('/api/auth', authRouter);
 app.use('/api/accounts', accountRouter);  
 app.use('/api/transactions', transactionRoutes)  
